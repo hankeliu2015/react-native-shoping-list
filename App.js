e9d649f2c3eb245d744277e65defad37670305e5
Hank Liu <hanke.liu@gmail.com>
2021-03-02 22:50:51 -0500

    add state hooks to handle shopping items and create ListItem component to render each flatlist render item

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import Header from './components/Header'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Header from './components/Header'; 
+import ListItem from './components/ListItem'
+// import { uuid } from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text:'Milk'}, 
+    {id: 2, text:'Egg'}, 
+    {id: 3, text:'Bread'}, 
+    {id: 4, text:'Bacon'}, 
+  ]);
 
-  
   return (
     <View style={Styles.container}>
       <Header title="Shopping List"/>
+      <FlatList 
+        data={items}
+        renderItem={({item}) => <ListItem item={item}/>} 
+        />
     </View>
   )
 }
